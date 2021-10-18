fc6567dde15c2ce30267b9eeb1016cdc76c7d26a
jdrever <james@careful.digital>
2021-10-18 11:13:59 +0100

    adding private links for schools

---
+++ _data/allstories.js
@@ -0,0 +1,30 @@
+const sanityClient = require('@sanity/client')
+const projectId = "3vgpwmyb"
+const apiToken = ''
+const client = sanityClient({
+  projectId,
+  dataset: 'production',
+  token: '',
+  useCdn: false
+})
+
+
+module.exports = async function () {
+  const query = `
+    *[ _type == "story" && !(_id in path("drafts.**")) ]{
+       "storyId" : _id,
+       name,
+       description,
+       who_created,
+       how_created,
+       "country": country->name,
+       "school": school->name,
+       tags,
+       "mainImageUrl": mainImage.asset->url,
+       content,
+    } | order(publishedAt desc)
+  `
+  const params = {}
+  
+  return await client.fetch(query, params)
+}
